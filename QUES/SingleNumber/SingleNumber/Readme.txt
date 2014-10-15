1c660fdd406b2edc5eb400b3c5ec7bd181b17bf5
Kerwong <nutsmail@163.com>
2014-10-15 09:11:24 +0800

    Single Number @ leetcode

---
+++ QUES/SingleNumber/SingleNumber/Readme.txt
@@ -0,0 +1,1 @@
+Q: Given an array of integers, every element appears twice except for one. Find that single one.
---
+++ QUES/SingleNumber/SingleNumber/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <hash_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        
+    }
+};
+
+int main() {
+	return 0;
+}
