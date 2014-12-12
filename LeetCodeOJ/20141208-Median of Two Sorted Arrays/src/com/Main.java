4e518c9933cd29f0fb04173f691172aeda27e4ee
Kerwong <nutsmail@163.com>
2014-12-12 17:16:05 +0800

    add

---
+++ LeetCodeOJ/20141208-Median of Two Sorted Arrays/src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+
+		int[] numbers = { 1, 2, 3, 4, 5, 7, 9 };
+		sol.findMedianSortedArrays(numbers, numbers);
+	}
+
+}
