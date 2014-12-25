a4f9f45ff829e04bebd47be08b51ae9cf58e47a8
Kerwong <nutsmail@163.com>
2014-12-25 19:14:22 +0800

    problems

---
+++ LeetCodeOJ/20141225-Maximum Product Subarray/src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Solution sol = new Solution();
+		
+		int[] num = {3,3,1,3};
+		
+		int x = sol.findMin(num);
+	}
+
+}
---
+++ LeetCodeOJ/20141225-Maximum Product Subarray/src/com/Solution.java
@@ -0,0 +1,17 @@
+package com;
+
+
+/**
+ * Find the contiguous subarray within an array (containing at least one number) 
+ * which has the largest product.
+ * 
+ * For example, given the array [2,3,-2,4],
+ * the contiguous subarray [2,3] has the largest product = 6.
+ */
+
+public class Solution {
+	/** SOLUTION 1 */
+    public int maxProduct(int[] A) {
+        	
+    }
+}
