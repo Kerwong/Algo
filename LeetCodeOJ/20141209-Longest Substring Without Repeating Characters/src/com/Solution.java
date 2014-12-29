4bac993b1102d6eb28ad6c49effa484656daeaf6
Kerwong <nutsmail@163.com>
2014-12-29 17:25:50 +0800

    2 problems

--- LeetCodeOJ/20141209-Longest Substring Without Repeating Characters/src/com/Solution.java
+++ LeetCodeOJ/20141209-Longest Substring Without Repeating Characters/src/com/Solution.java
@@ -1,6 +1,8 @@
 package com;
 
 import java.util.HashMap;
+import java.util.HashSet;
+
 
 /**
  * Given a string, find the length of the longest substring without repeating 
@@ -11,6 +13,32 @@ import java.util.HashMap;
 
 public class Solution {
 	public int lengthOfLongestSubstring(String s) {
+		
+		HashMap<Character, Integer> hMap = new HashMap<Character, Integer>();
+		
+		int index = -1;
+		int maxLen = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			Character ch = s.charAt(i);
+			if (!hMap.containsKey(ch)) {
+				hMap.put(ch, i);
+				maxLen++;
+			} else {
+				int tmp = hMap.get(ch);
+				if(i - tmp > maxLen) {
+					if (tmp > index) {
+						index = tmp;
+						maxLen = i - tmp;
+					}
+				}
+				
+				if (tmp > index) {
+					index = tmp;
+				}
+				hMap.put(ch, i);
+			}
+		}
         return 0;
     }
 }
