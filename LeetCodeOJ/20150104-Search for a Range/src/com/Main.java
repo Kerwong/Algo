77daa4cfcdcbdcc3cc0b2340fd68a9ec3e37dca8
Kerwong <nutsmail@163.com>
2015-01-04 18:13:17 +0800

    solver 6 problems per day, nice

---
+++ LeetCodeOJ/20150104-Search for a Range/src/com/Main.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution solution = new Solution();
+		
+
+		//int[] A = {7,10,12,15,0,1,3,4,6};
+		int[] A = {1,3,5};
+		
+	}
+}
